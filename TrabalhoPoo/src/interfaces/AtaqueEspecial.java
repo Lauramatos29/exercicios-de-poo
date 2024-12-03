36b13b1ac497eec5df01891242b70e9e926caede
Lauramatos29 <lauramatos051@gmail.com>
2024-12-03 11:10:29 -0300

    Add files via upload

---
+++ TrabalhoPoo/src/interfaces/AtaqueEspecial.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+import classes.Pokemon;
+
+public interface AtaqueEspecial {
+	
+	void ataqueKabush(Pokemon oponente, int ataque);
+	void ataqueBambam(Pokemon oponente, int ataque);
+	void ataquePowPow(Pokemon oponente, int ataque);
+	void ataqueChabam(Pokemon oponente, int ataque);
+
+}
